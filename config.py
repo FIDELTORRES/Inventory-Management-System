c4db0fcbaf1f01f20bcd1bff466342ad3f95e838
FIDEL <fidel.torres.ipanaque@gmail.com>
2026-07-22 11:48:39 -0500

    Inicio del desarrollo

--- config.py
+++
@@ -1,8 +0,0 @@
-import os
-from dotenv import load_dotenv
-load_dotenv()
-
-class Config:
-    SECRET_KEY = os.getenv("SECRET_KEY", "change_this_secret")
-    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URI", "sqlite:///inventory.db")
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
